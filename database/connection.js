09f81fe1b40490b218ca6ed1c21f2d02aadeef12
Adanpradana <adanizkahariz@gmail.com>
2023-02-12 10:45:52 +0700

    add database seed

---
+++ database/connection.js
@@ -0,0 +1,21 @@
+const { v4: uuidv4 } = require("uuid");
+const users = [
+  {
+    id: uuidv4(),
+    name: "adan",
+    role: "admin",
+    password: "@12334",
+  },
+  {
+    id: uuidv4(),
+    name: "john doe",
+    role: "developer",
+    password: "11234",
+  },
+  {
+    id: uuidv4(),
+    name: "riska",
+    role: "supervisor",
+    password: "@12334",
+  },
+];
