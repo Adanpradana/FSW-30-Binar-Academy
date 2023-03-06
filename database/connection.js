bd7cbd8f1dfcdd577e07f453f495f14395dd25f6
Adanpradana <adanizkahariz@gmail.com>
2023-03-07 01:14:36 +0700

    delete unused file

--- database/connection.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require("sequelize");
-require("dotenv").config;
-
-const db = new Sequelize("binar_games", "lenovo", "adan1307", {
-  host: "localhost",
-  dialect: "postgres",
-});
-
-module.exports = db;
